1c5a43a9805b5f88ab254bc951c03ac7b74fc738
DTUSHAR <tusharsahu3627@gmail.com>
2021-07-06 19:37:57 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        <div className="cover">
+            <div className="data">
+                <p>Name   :</p>
+                <p>age    :</p>
+                <p>Gender :</p>
+                <p>DOB    :</p>
+            </div>
+            <div>
+                <p> MAXX</p>
+                <p> 21 </p>
+                <p> Male</p>
+                <p> 09/04/2000</p>
+            </div>
+
+        </div>
+        <button onClick={() => themeToggler()}>Change Theme</button>
+        
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
